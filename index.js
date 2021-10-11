13321c201ed8140d7ecd8710b8889e600c9a34dd
megafriday <megafriday@yahoo.co.jp>
2021-10-11 22:11:51 +0900

    [add] #16 3章12節「HTTPサーバー」練習問題のコーディング

--- index.js
+++ index.js
@@ -2,10 +2,15 @@
 const http = require('http');
 const server = http.createServer((req, res) => {
   res.writeHead(200, {
-    'Content-Type': 'text/plain; charset=utf-8'
+    'Content-Type': 'text/html; charset=utf-8'
   });
-  res.write(req.headers['user-agent']);
-  res.end();
+  res.write(
+    `<!DOCTYPE html><html lang="ja">
+      <body>
+        <h1>HTMLの一番大きい見出しを表示します</h1>
+      </body>
+    </html>`
+  ); res.end();
 });
 const port = 8000;
 server.listen(port, () => {
