1d8674f1fc673f98da7e78bf7c76e7deaacb72dc
nyeoro-bong <68091561+nyeoro-bong@users.noreply.github.com>
2021-10-20 21:14:58 +0900

    GET, POST & DELETE の各メソッドに対応するswitch文を実装しました

--- index.js
+++ index.js
@@ -1,12 +1,42 @@
 'use strict';
 const http = require('http');
-const server = http.createServer((req, res) => {
-  res.writeHead(200, {
-    'Content-Type': 'text/plain; charset=utf-8'
+const server = http
+  .createServer((req, res) => {
+    const now = new Date();
+    console.info('[' + now + '] Requested by ' + req.socket.remoteAddress);
+    res.writeHead(200, {
+      'Content-Type': 'text/plain; charset=utf-8'
+    });
+
+    switch (req.method) {
+      case 'GET':
+        res.write('GET' + req.url);
+        break;
+      case 'POST':
+        res.write('POST' + req.url);
+        let rawData = '';
+        req
+          .on('data', chunk => {
+            rawData = rawData + chunk;
+          })
+          .on('end', () => {
+            console.info('[' + '] Data posted: ' + rawData);
+          });
+        break;
+      case 'DELETE':
+        res.write('DELETE' + req.url);
+        break;
+      default:
+        break;
+    }
+    res.end();
+  })
+  .on('error', e => {
+    console.error('[' + new Date() + '] Server Error', e);
+  })
+  .on('clientError', e => {
+    console.error('[' + new Date() + '] Client Error', e);
   });
-  res.write(req.headers['user-agent']);
-  res.end();
-});
 const port = 8000;
 server.listen(port, () => {
   console.log('Listening on ' + port);
