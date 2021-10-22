32fe682d356eef65803297b99f2197c37d4adda7
satsukizzz <dn.asp01@gmail.com>
2021-10-22 22:50:07 +0900

    ブラウザ用JS, CSSの読み込み

--- index.js
+++ index.js
@@ -4,6 +4,42 @@ const pug = require('pug');
 const fs = require('fs');
 
 const server = http.createServer((req, res) => {
+  const urlDirectories = req.url.split('/');
+  // URLによってJSやCSSの振り分け
+  if (urlDirectories[1] === 'css') {
+    // ブラウザ用CSS読み込み
+    try {
+      const cssFile = fs.readFileSync('./resources/css/' + urlDirectories[2]);
+      res.writeHead(200, {
+        'Content-Type': 'text/css charset=utf-8'
+      });
+      res.write(cssFile);
+    } catch (e) {
+      //TODO client errorの追加
+      console.log(e);
+    } finally {
+      res.end();
+      return;
+    }
+  } else if (urlDirectories[1] === 'js') {
+    // ブラウザ用JS読み込み
+    try {
+      const jsFile = fs.readFileSync('./resources/js/' + urlDirectories[2]);
+      res.writeHead(200, {
+        'Content-Type': 'text/javascript charset=utf-8'
+      });
+      res.write(jsFile);
+    } catch (e) {
+      //TODO client errorの追加
+      console.error(e);
+    } finally {
+      res.end();
+      return;
+    }
+  }
+
+
+  // 以降、ページやリダイレクトの読み込み
   if (req.url === '/calculator') {
     res.writeHead(200, {
       'Content-Type': 'text/html; charset=utf-8'
