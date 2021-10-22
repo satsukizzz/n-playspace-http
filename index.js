0ddc0a6650073120001f12a1e8523bc06139092c
satsukizzz <dn.asp01@gmail.com>
2021-10-22 22:49:09 +0900

    電卓ページの仮作成

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 'use strict';
 const http = require('http');
+const pug = require('pug');
+const fs = require('fs');
+
 const server = http.createServer((req, res) => {
+  if (req.url === '/calculator') {
+    res.writeHead(200, {
+      'Content-Type': 'text/html; charset=utf-8'
+    });
+    res.write(pug.renderFile('./resources/calculator.pug'));
+    res.end();
+    return;
+  }
+
+  // デフォルトぺージ
   res.writeHead(200, {
     'Content-Type': 'text/html; charset=utf-8'
   });
@@ -10,7 +23,9 @@ const server = http.createServer((req, res) => {
         <h1>HTMLの一番大きい見出しを表示します</h1>
       </body>
     </html>`
-  ); res.end();
+  );
+  res.end();
+  return;
 });
 const port = 8000;
 server.listen(port, () => {
