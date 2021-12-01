14d1f46aafc6f74f6392efa3b505417b8f4161f6
Kento Nakamura <kento.nakamura2009@gmail.com>
2021-12-01 19:28:13 +0900

    ３章１２節のコーディング

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 'use strict';
 const http = require('http');
 const server = http.createServer((req, res) => {
+  console.info(
+    '[' + new Date() + '] Requested by ' + req.socket.remoteAddress
+  );
   res.writeHead(200, {
-    'Content-Type': 'text/html; charset=utf-8'
+    'Content-Type': 'text/plain; charset=utf-8'
   });
-  res.write(
-    `<!DOCTYPE html><html lang="ja">
-      <body>
-        <h1>HTMLの一番大きい見出しを表示します</h1>
-      </body>
-    </html>`
-  ); res.end();
-});
+    res.write(req.headers['user-agent']);
+    res.end();
+})
+  .on('error', e => {
+    console.error('[' + new Date() + '] Server Error', e);
+  })
+  .on('clientError', e => {
+    console.error('[' + new Date() + '] Client Error', e);
+  });
 const port = 8000;
 server.listen(port, () => {
   console.log('Listening on ' + port);
